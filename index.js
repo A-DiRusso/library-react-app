815838e59941d9b351648000f313675aa545af8b
Anthony DiRusso <ossurid@gmail.com>
2019-04-22 11:28:31 -0400

    adds packages and creates elements

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const h = React.createElement
+
+
+let h1 = h('h1', null, 'Bookstore');
+let li = h('li', null, 'Books are coming soon');
+let ul = h('ul', { className: 'book-list' }, li);
+let footer = h('footer', null, 'copyright');
+
+
+let main = h('main', null [
+    h1,
+    ul,
+    footer
+]);
+
+ReactDOM.render(main, document.querySelector('.react-root'));
